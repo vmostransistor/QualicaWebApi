b2595c233e1c0b3bf2865c8b2846543a6bef6e26
Pieter Lombaard <pajlombaard@gmail.com>
2018-08-28 15:51:10 +0200

    Add files via upload

---
+++ Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Qualica.Models
+{
+    public class Transaction
+    {
+        public int ID { get; set; }
+        public string Reference { get; set; }
+        public DateTime Transaction_date { get; set; }
+        public double Amount { get; set; }
+        public Guid From_Account { get; set; }
+        public Guid To_Account { get; set; }
+    }
+}
