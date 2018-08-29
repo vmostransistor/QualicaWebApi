b870636f3e9d1612ec09b40e98c50a080372e79f
Pieter Lombaard <pajlombaard@gmail.com>
2018-08-29 11:13:19 +0200

    Update TransactionController.cs

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
+using Newtonsoft.Json;
+using Qualica.Models;
 using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using System.Web.Http.Description;
-using Qualica.Models;
-using Newtonsoft.Json;
 
 namespace Qualica.Controllers
 {
