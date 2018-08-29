114f619adf39517f8ba9324773221ddd79ab547e
Pieter Lombaard <pajlombaard@gmail.com>
2018-08-29 11:14:12 +0200

    Update Configuration.cs

--- Migrations/Configuration.cs
+++ Migrations/Configuration.cs
@@ -1,9 +1,7 @@
 namespace Qualica.Migrations
 {
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<Qualica.Models.QualicaContext>
     {
@@ -16,7 +14,7 @@ namespace Qualica.Migrations
         {
             //  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data. E.g.
             //
             context.Transactions.AddOrUpdate(x => x.ID,
